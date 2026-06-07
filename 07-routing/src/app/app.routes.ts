6c1e2bcc592d7bc8c99a0a14280381a5e19f68c4
swastisunder <badatyaswastisunder@gmail.com>
2026-06-07 14:51:09 +0530

    07-routing

--- 07-routing/src/app/app.routes.ts
+++ 07-routing/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
 import { Login } from './login/login';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
   { path: 'login', component: Login },
--- 07-routing/src/app/app.ts
+++ 07-routing/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
