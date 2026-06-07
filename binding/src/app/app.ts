dabc2eed65f81fe2a092f3e7209e3fd848d8788c
swastisunder <badatyaswastisunder@gmail.com>
2026-06-07 12:51:17 +0530

    binding

---
+++ binding/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  name = 'OMG';
+}
